2dcab7303e9d61bbd9b6088045b47eb86c60c349
kennethkimachia <114845663+kennethkimachia@users.noreply.github.com>
2024-11-04 23:24:49 +0300

    Fixed some bugs

--- lib/pocketbase.ts
+++ lib/pocketbase.ts
@@ -1,5 +1,5 @@
 import PocketBase from 'pocketbase';
 
-const pb = new PocketBase(process.env.POCKETBASE_URL);
+const pb = new PocketBase(process.env.NEXT_PUBLIC_POCKETBASE_URL);
 
 export default pb; 
